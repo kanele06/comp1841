c44bd06d50422c031fb71840bc22c6faef5717a5
kanele06 <lengockimngan11102006@gmail.com>
2026-05-22 12:36:59 +0700

    Second commit

---
+++ week2/welcome/templates/form.html.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Welcome form</title>
+</head>
+<body>
+	<form action="" method="post">
+		<label for="firstname">First name:
+		<input type="text" name="firstname" id="firstname"></label><br />
+        <label for="lastname">Last name:
+		<input type="text" name="lastname" id="lastname"></label><br />
+		<input type="submit" value="GO">
+	</form>
+</body>
+</html>
