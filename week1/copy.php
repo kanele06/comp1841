4138ee0b41dc2b0fb4c3bfd8ddcbc2a1eafdd1d8
kanele06 <lengockimngan11102006@gmail.com>
2026-05-22 09:07:27 +0700

    First commit

---
+++ week1/copy.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dynamic Copyright</title>
+</head>
+<body>
+&copy; 2010 - <?php echo date("Y")?>
+</body>
+</html>
---
+++ week1/single_double.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Single-Double</title>
+</head>
+<body>
+<?php
+$price = 20;
+
+echo "$price";
+echo "<br />";
+echo '$price';
+?>
+</body>
+</html>
