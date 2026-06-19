f6874136355fc16b4916ed5e529b313f8f714ae5
kanele06 <lengockimngan11102006@gmail.com>
2026-06-19 12:18:18 +0700

    First commit

---
+++ week6_1/templates/jokes.html.php
@@ -0,0 +1,54 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>List of jokes</title>
+    </head>
+    <body>
+
+    <?php if (isset($error)): ?>
+
+        <p><?= $error ?></p>
+
+    <?php else: ?>
+
+        <table border="2px">
+            <thead>
+                <tr>
+                <th>Joke Title</th>
+                <th>Category
+                <th>Action</th>
+                </tr>
+            </thead>
+            
+            <?php foreach ($jokes as $joke): ?>
+                <tr>
+                    <td width="400px">
+                        <?= htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8') ?>
+
+                        (by <a href="mailto:<?=htmlspecialchars($joke['email'], ENT_QUOTES, 'UTF-8'); ?>">
+                            <?= htmlspecialchars($joke['name'], ENT_QUOTES, 'UTF-8'); ?></a>)
+                    </td>
+
+                    <td width="300px">
+                        <?= htmlspecialchars($joke['category'], ENT_QUOTES, 'UTF-8') ?>
+
+                    </td>
+                    <td width="50px">
+                        <form action="deletejoke.php" method="post">
+                            <input type="hidden" name="id" value="<?= htmlspecialchars($joke['id'], ENT_QUOTES, 'UTF-8')?>">
+                            <input type="submit" value="Delete">
+                        </form>
+                    </td>
+
+                </tr>
+            <?php endforeach; ?>
+        </table>
+
+    <?php endif; ?>
+
+    </body>
+    </html>
+                
---
+++ week6_2/templates/addjoke.html.php
@@ -0,0 +1,9 @@
+<form action="" method="post">
+    <label for='joketext'> Type your joke here;</label>
+    <textarea name="joketext" rows="3" cols="40"></textarea>
+
+    <label for="image">Type image file name:</label>
+    <input type="text" name="image" id="image">
+
+    <input type="submit" name="submit" value="Add">
+</form>
