79bf2726ed14e3c59c09a3fe380903715559310b
kanele06 <lengockimngan11102006@gmail.com>
2026-06-19 12:17:28 +0700

    First commit

---
+++ week6_3/jokes.php
@@ -0,0 +1,26 @@
+<?php
+try{
+    include 'includes/DatabaseConnection.php';
+    $sql = 'SELECT joke.id,
+               joketext,
+               joke.image,
+               joke.jokedate,
+               author.name,
+               email,
+               category.name AS category
+        FROM joke
+        INNER JOIN author
+            ON joke.authorid = author.id
+        INNER JOIN category
+            ON joke.categoryid = category.id';
+    $jokes = $pdo->query($sql);
+    $title = 'Joke List';
+
+    ob_start();
+    include 'templates/jokes.html.php';
+    $output = ob_get_clean();
+} catch (PDOException $e) {
+    $title = 'An error has occured';
+    $output = 'Database Error:' . $e->getMessage();
+}
+include 'templates/layout.html.php';
